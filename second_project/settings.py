00295448c05029691fe74075d3c89c3a1416e591
cooperama <coope133@gmail.com>
2020-10-30 16:53:03 -0700

    Adds home view & path

--- second_project/settings.py
+++ second_project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'second_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': traveldb,
+        'NAME': 'traveldb',
     }
 }
 
