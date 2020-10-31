0c11a2c373e0de89f1be90cbf1624c97e307755c
cooperama <coope133@gmail.com>
2020-10-30 17:22:15 -0700

    Adds Profile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    current_city = models.CharField(max_length=100)
+    join_date = models.DateField(auto_now_add=True)
+    past_cities = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
