7a4c2e1fa28cd12cd05c7e6eeea0fad6ce1bbb97
cooperama <coope133@gmail.com>
2020-10-30 17:38:56 -0700

    Sets up login capabilities

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
@@ -6,6 +9,8 @@ class Profile(models.Model):
     current_city = models.CharField(max_length=100)
     join_date = models.DateField(auto_now_add=True)
     past_cities = models.TextField(blank=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
