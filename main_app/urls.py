e9eb3e0b830f1e7c91c0f3d9ca34e8003c09991a
chriszalapa <Chriszalapa@gmail.com>
2020-11-02 13:34:33 -0800

    Updates

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     # Profile/User urls
     path('profile/', views.profile, name='profile'),
+    path('profile/new/', views.add_profile, name='add_profile'),
     path('registration/signup/', views.signup, name='signup'),
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import ProfileForm
 
 
 # --------------------------- STATIC PAGES
@@ -16,9 +18,28 @@ def about(request):
 # --------------------------- PROFILE 
 @login_required
 def profile(request):
+    profile = Profile.objects.filter(user=request.user)
+    context = {
+        'profile': profile
+    }
 
-    return render(request, 'profile.html')
+    return render(request, 'profiles/index.html', context)
 
+
+def add_profile(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST)
+        if profile_form.is_valid():
+            new_profile = profile_form.save(commit=False)
+            new_profile.user = request.user
+            new_profile.save()
+            return redirect('profile')
+    else:
+        profile_form = ProfileForm()
+        context = {
+            'profile_form': profile_form
+        }
+        return render(request, 'profiles/new.html', context)
 
 def signup(request):
     error_message = ''
