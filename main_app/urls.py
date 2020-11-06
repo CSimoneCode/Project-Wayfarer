562f06d567ccfd8b1c70d3fd63890ebfb7063d79
cooperama <coope133@gmail.com>
2020-11-05 20:34:26 -0800

    Committing to pull

--- main_app/urls.py
+++ main_app/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('registration/signup/', views.signup, name='signup'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
