1c6a8bb25fac4e57c452524a0965260ee738627d
csimonecode <csimonecode@gmail.com>
2020-12-17 08:42:05 -0700

    preparing for deployment, installed deployment dependencies (gunicorn, heroku-on-django, whitenoise)

--- second_project/settings.py
+++ second_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'l@&6wegb^a)-o8f^p+-dr$yutwtu1yt_9wa1w4@1gof*mhwyy('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -143,3 +144,5 @@ EMAIL_HOST_PASSWORD = 'partyJESUS4lif3'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+django_on_heroku.settings(locals())
