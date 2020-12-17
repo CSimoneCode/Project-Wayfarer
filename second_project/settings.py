34fd93caee2e5c7960783012336575e2d78ad73e
csimonecode <csimonecode@gmail.com>
2020-12-17 09:36:28 -0700

    tweaked some settings for deployment

--- second_project/settings.py
+++ second_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_on_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,4 +142,4 @@ EMAIL_HOST_PASSWORD = 'partyJESUS4lif3'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
